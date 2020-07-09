e7c6df7dbb3211a58c51d5c73aa0386475c20634
duoacoder <duoacoder@gmail.com>
2020-07-09 11:10:56 +0530

    config changes

--- config/default.js
+++ config/default.js
@@ -1,70 +1,58 @@
 module.exports = {
-
-
-    "Redis":
-        {
-            "mode": "sentinel",//instance, cluster, sentinel
-            "ip": "45.55.142.207",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
-            "redisDB": 8,
-            "sentinels": {
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port": 16389,
-                "name": "redis-cluster"
-            }
-
-        },
-
-
-    "Security":
-        {
-
-            "ip": "45.55.142.207",
-            "port": 6389,
-            "user": "duo",
-            "password": "DuoS123",
-            "mode": "sentinel",//instance, cluster, sentinel
-            "sentinels": {
-                "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
-                "port": 16389,
-                "name": "redis-cluster"
-            }
-        },
-
-
-    "Host":
-        {
-            "resource": "cluster",
-            "vdomain": "127.0.0.1",
-            "domain": "127.0.0.1",
-            "port": "3637",
-            "version": "1.0.0.0"
-        },
-
-    "LBServer": {
-
-        "ip": "192.168.0.101",
-        "port": "3636"
-
+  Redis: {
+    mode: "instance", //instance, cluster, sentinel
+    ip: "",
+    port: 6379,
+    user: "",
+    password: "",
+    redisDB: 8,
+    sentinels: {
+      hosts: "",
+      port: 16389,
+      name: "redis-cluster",
     },
+  },
 
-    "Mongo":
-        {
-            "ip": "104.236.231.11",
-            "port": "27017",
-            "dbname": "dvpdb",
-            "password": "DuoS123",
-            "user": "duo"
-        },
+  Security: {
+    ip: "",
+    port: 6379,
+    user: "",
+    password: "",
+    mode: "instance", //instance, cluster, sentinel
+    sentinels: {
+      hosts: "",
+      port: 16389,
+      name: "redis-cluster",
+    },
+  },
 
-    "Services": {
-        "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
-        "resourceServiceHost": "resourceservice.app.veery.cloud",
-        "resourceServicePort": "8831",
-        "resourceServiceVersion": "1.0.0.0",
+  Host: {
+    resource: "cluster",
+    vdomain: "127.0.0.1",
+    domain: "127.0.0.1",
+    port: "3637",
+    version: "1.0.0.0",
+  },
 
-    }
+  LBServer: {
+    ip: "",
+    port: "",
+  },
 
+  Mongo: {
+    ip: "104..231.11",
+    port: "",
+    dbname: "",
+    password: "",
+    user: "",
+    type: "mongodb+srv",
+  },
+
+  Services: {
+    accessToken:
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
+    resourceServiceHost: "resourceservice.app.veery.cloud",
+    resourceServicePort: "8831",
+    resourceServiceVersion: "1.0.0.0",
+  },
 };
